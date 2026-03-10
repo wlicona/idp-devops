8fd5d77e57a48ca2d422b32da7f21bfb0bdb7ad3
william Licona <william.licona115@outlook.com>
2026-03-09 21:34:45 -0500

    Se modifica el estilo del register y se modifican las rutas para crear los repos

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
 from app.models import user
@@ -11,8 +12,20 @@ from app.routes import github
 
 app = FastAPI(title= "DevOps IDP Platform", version="1.0")
 
-
 Base.metadata.create_all(bind=engine)
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(github.router, prefix="/github", tags=["github"])
