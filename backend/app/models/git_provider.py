91bb7ab7d1a8eade3c9b45b23c03892f526a1366
william Licona <william.licona115@outlook.com>
2026-03-19 14:47:19 -0500

    se crea la entidad de repositorio para guardarlo en la BD

---
+++ backend/app/models/git_provider.py
@@ -0,0 +1,11 @@
+import uuid
+from sqlalchemy import Column,String
+from sqlalchemy.dialects.postgresql import UUID
+from app.database import Base
+
+class provider(Base):
+    __tablename__="git_providers"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String)
+    api_url = Column(String)
