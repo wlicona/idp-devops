6932fe89ef5fdc09fa8c054600d0a063e79fbb35
william Licona <william.licona115@outlook.com>
2026-03-28 14:50:40 -0500

    se actualiza rutas

--- backend/app/routes/repository.py
+++ backend/app/routes/repository.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import func
@@ -93,31 +92,4 @@ def get_repositories(
         })
 
     return result
-=======
-from fastapi import APIRouter, Depends
-
-from app.schemas.repository import repoCreate
-from app.services.github_service import create_repo_github
-from app.services.gitlab_service import create_repo_gitlab
-from app.services.bitbucket_service import create_repo_bitbucket
-from app.core.dependencies import get_current_user
-
-router = APIRouter()
-
-@router.post("/repositories")
-def create_repo(repo: repoCreate, user= Depends(get_current_user)):
-
-
-    if repo.provider == "github":
-        return create_repo_github(repo.name, repo.description)
-    
-    if repo.provider == "gitlab":
-        return create_repo_gitlab(repo.name, repo.description)
-    
-    if repo.provider == "bitbucket":
-        return create_repo_bitbucket(repo.name, repo.description)
-    
-    return {"error: Invalid Provider"}
-
->>>>>>> 3ab4f045b3ad40d1dbe1fe46c229f9bab5435cd1
 
